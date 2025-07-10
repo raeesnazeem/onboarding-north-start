64bf0b5d22fcde8487495a00dc91f9201f734c11
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-07-10 12:36:55 +0530

    update: appId

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -11,5 +11,7 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'nl6ql277xv5dwz16wr6e6mch'
+    
   }
 })
